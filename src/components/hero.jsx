7d8c0ac2142a7596b94ecfd7749eaee15b52b962
SaurabhS78 <saurabhsangam78@gmail.com>
2021-06-06 15:32:34 +0530

    feat: responsive hero section

---
+++ src/components/hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import heroImage from "assets/images/mainbuilding.png";
+
+function HeroSection(props) {
+  return (
+    <section className="hero--main d-flex align-items-center flex-wrap">
+      <span className="hero--bg-dec" />
+      <div className="hero--content">{props.children}</div>
+      <img className="hero--img" src={heroImage} />
+    </section>
+  );
+}
+
+export default HeroSection;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,15 @@
+import HeroSection from "components/hero";
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <HeroSection>
+      <div className="home--heading">
+        <span>WELCOME TO THE</span> <br />
+        <h1>Department of Humanities & Social Sciences</h1>
+      </div>
+    </HeroSection>
+  );
 }
 
 export default Home;
